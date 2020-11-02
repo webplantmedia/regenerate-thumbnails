3acd59d4dcb4857b7fe0423608f5cc97896da23d
Chris Baldelomar <chris@webplant.media>
2020-11-02 11:45:10 -0600

    update

--- regenerate-thumbnails.php
+++ regenerate-thumbnails.php
@@ -5,7 +5,7 @@
 Plugin Name:  Regenerate Thumbnails
 Description:  Regenerate the thumbnails for one or more of your image uploads. Useful when changing their sizes or your theme.
 Plugin URI:   https://alex.blog/wordpress-plugins/regenerate-thumbnails/
-Version:      3.1.3
+Version:      9.1.3
 Author:       Chris Baldelomar
 Author URI:   https://alex.blog/
 Text Domain:  regenerate-thumbnails
